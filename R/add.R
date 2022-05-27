f7388128696c3fb3dda3289e3f68ee54f58e4d8d
Ramnath Vaidyanathan <ramnath@thetaequity.com>
2022-05-26 18:21:58 -0700

    feat: add function - add
    Add two numbers

---
+++ R/add.R
@@ -0,0 +1,13 @@
+#' Add two numbers
+#'
+#' @param x The first number
+#' @param y The second number
+#'
+#' @return The sum of the two numbers
+#' @export
+#'
+#' @examples
+#' add(1, 2)
+add <- function(x, y){
+  x + y
+}
---
+++ tests/testthat/test-add.R
@@ -0,0 +1,4 @@
+test_that("addition works", {
+  expect_equal(add(1, 2), 3)
+  expect_equal(add(4, 5), 9)
+})
